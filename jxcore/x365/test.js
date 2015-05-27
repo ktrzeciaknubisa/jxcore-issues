8dfded35b0009da673f1f402bc11dfb9829f2f19
nubisa_krzs <Gitt#Hub12>
2015-05-27 14:39:48 +0200

    updates

--- jxcore/x365/test.js
+++ jxcore/x365/test.js
@@ -3,15 +3,23 @@
 var task = {};
 task.define = function () {
 
+  jxcore.utils.console.warn("pid", process.threadId);
   try {
     // this works:
     // var mod = require(__dirname + "/_asset_file.js");
 
     // this fails:
-    var mod = require("./_asset_file.js");
+    //var mod = require("./_asset_file.js");
+    //console.log("process.cwd() 2",process.cwd());
+    var fs = require("fs");
+    //var fs = require(__dirname + "/fs1.js");
+    var stat = fs.statSync("./_asset_file.js");
+    jxcore.utils.console.log("stat2", stat, "yellow");
+
   } catch (ex) {
     console.error("Cannot require ./_asset_file.js from inside define(): " + ex + "\n__dirname = " + __dirname+
     "\n__process.cwd() = " + process.cwd());
+    console.error(ex.stack);
     process.exit(1);
   }
 };
@@ -19,5 +27,10 @@ task.define = function () {
 jxcore.tasks.addTask(task);
 
 
-//console.log("process.cwd()",process.cwd());
+//console.log("process.cwd() 1",process.cwd());
+//
+//var fs = require("fs");
+//var fs = require(__dirname + "/fs1.js");
+//var stat = fs.statSync("./_asset_file.js");
+//jxcore.utils.console.log("stat1", stat, "green");
 
