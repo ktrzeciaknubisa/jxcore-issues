755995128d89597e1a569e6cd41a39d09b8ef813
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-06-09 19:46:58 +0200

    x387

---
+++ jxcore/x387/test.js
@@ -0,0 +1,6 @@
+// Copyright & License details are available under JXCORE_LICENSE file
+
+
+var folder = "Эа_эжт_аляквюид/test";
+var ret = jxcore.utils.cmdSync("mkdir -p " + folder);
+console.log(ret.out);
