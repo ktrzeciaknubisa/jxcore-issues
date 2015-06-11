f73172b87c8a44372577f309bcd32eb6b277ac8b
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-06-11 10:36:41 +0200

    updates mac

--- jxcore/x387/test.js
+++ jxcore/x387/test.js
@@ -1,6 +1,5 @@
 // Copyright & License details are available under JXCORE_LICENSE file
 
 
-var folder = "Эа_эжт_аляквюид/test";
-var ret = jxcore.utils.cmdSync("mkdir -p " + folder);
+var ret = jxcore.utils.cmdSync("mkdir -p Эа_эжт_аляквюид/test");
 console.log(ret.out);
