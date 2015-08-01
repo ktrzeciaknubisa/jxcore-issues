d8621630669ffb89efc8378bf9b31d4c77b6755e
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-08-01 14:35:32 +0200

    from mac

---
+++ jxcore/_new/index.js
@@ -0,0 +1,7 @@
+
+
+console.log("process.__npmjxpath", process.__npmjxpath);
+console.log("process.env.HOME", process.env.HOME);
+console.log("process.env.HOMEPATH", process.env.HOMEPATH);
+console.log("process.env.USERPROFILE", process.env.USERPROFILE);
+
