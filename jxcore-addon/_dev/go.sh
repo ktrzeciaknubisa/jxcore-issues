fe4c279323fcf310d337242e71774d6f60058984
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-08-12 18:57:25 +0200

    from mac

---
+++ jxcore-addon/_dev/go.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+BIN=jxsm
+if [[ $1 != "" ]]; then BIN=$1; fi
+if [[ $1 == "v8" ]]; then BIN=jxv8; fi
+#rm -rf ./jxcore-addon/
+if [[ ! -d jxcore-addon ]]; then
+    git clone https://github.com/jxcore/jxcore-addon
+fi
+
+cd ./jxcore-addon/
+#rm -rf ~/.jx ~/.node-gyp build
+rm -rf build
+$BIN -e "jxcore.utils.console.info('installing: $BIN install')"
+$BIN install
+
+$BIN -e "jxcore.utils.console.info('executing')"
+$BIN test.js
+
+
+$BIN -e "jxcore.utils.console.info('Packing')"
+$BIN package test.js -add jxcore-addon
+
+$BIN package test.js -add jxcore-addon -native
+
+rm -rf ../out/*
+mkdir -p ../out
+mv test.jx test ../out
+
+
+cd ..
---
+++ jxcore-addon/_dev/pack.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+BIN=jxsm
+if [[ $1 != "" ]]; then BIN=$1; fi
+if [[ $1 == "v8" ]]; then BIN=jxv8; fi
+
+cd ./jxcore-addon/
+
+$BIN -e "jxcore.utils.console.info('Packing')"
+#$BIN package test.js -add jxcore-addon -extract "{ \"what\" : \"*.node\" }"
+#$BIN package test.js -add jxcore-addon --extract-what "*.node,*.js,*.md" --extract-where1 "cwd"
+#$BIN package test.js -add jxcore-addon -extract -extract-where "osiem"
+#$BIN package test.js -add jxcore-addon -extract -extract-where "cwd"
+
+#$BIN package test.js -add jxcore-addon -extract "{ \"what\" : \"*.node\" }" -native
+#$BIN package test.js -add jxcore-addon --extract-what "*.node,*.js,*.md" --extract-where1 "cwd" -native
+#$BIN package test.js -add jxcore-addon -extract -extract-where "osiem" -native
+#$BIN package test.js -add jxcore-addon -extract -extract-where "cwd" -native
+$BIN package test.js -add jxcore-addon -extract -native
+
+rm -rf ../out/*
+mkdir -p ../out
+mv test.jx test ../out
+#mv test.jx ../out
+
+
+cd ..
