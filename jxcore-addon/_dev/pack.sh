a4e8e24f1224cc070207657439fbe1f410420318
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-08-13 13:43:33 +0200

    from mac

--- jxcore-addon/_dev/pack.sh
+++ jxcore-addon/_dev/pack.sh
@@ -6,20 +6,22 @@ if [[ $1 == "v8" ]]; then BIN=jxv8; fi
 
 cd ./jxcore-addon/
 
-$BIN -e "jxcore.utils.console.info('Packing')"
+$BIN -e "jxcore.utils.console.info('Packing', 'clear')"
 
-$BIN compile test.jxp
+#$BIN compile test.jxp
 
-#$BIN package test.js  -extract "{ \"what\" : \"*.node\" }"
-#$BIN package test.js -add jxcore-addon --extract-what "*.node,*.js,*.md" --extract-where1 "cwd"
+#$BIN package test.js  -extract "{ \"what\" : \"*.node\" }" --extract-where:cwd
+#$BIN package test.js -add jxcore-addon --extract-what "*.node,*.js,*.md" --extract-where1 "dir"
 #$BIN package test.js -add jxcore-addon -extract -extract-where "osiem"
-#$BIN package test.js -add jxcore-addon -extract -extract-where "cwd"
+#$BIN package test.js -add jxcore-addon -extract -extract-where "./"
+#$BIN package test.js -add jxcore-addon -extract
 
 #$BIN package test.js -add jxcore-addon -extract "{ \"what\" : \"*.node\" }" -native
-#$BIN package test.js -add jxcore-addon --extract-what "*.node,*.js,*.md" --extract-where1 "cwd" -native
+#$BIN package test.js -add jxcore-addon --extract-what "*.node,*.js,*.md" --extract-where1 "dir" -native
 #$BIN package test.js -add jxcore-addon -extract -extract-where "osiem" -native
-#$BIN package test.js -add jxcore-addon -extract -extract-where "cwd" -native
-#$BIN package test.js -add jxcore-addon -extract -native
+#$BIN package test.js -add jxcore-addon -extract -extract-where:./ -native
+#$BIN package test.js -add jxcore-addon -extract-where:./ -native
+$BIN package test.js -add jxcore-addon -extract -native
 
 rm -rf ../out/*
 mkdir -p ../out
