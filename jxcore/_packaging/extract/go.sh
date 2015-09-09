c06a8aac30de180a53c2401dcb26ac3af2e0ada3
nubisa_krzs <Gitt#Hub12>
2015-09-09 16:45:09 +0200

    from win

--- jxcore/_packaging/extract/go.sh
+++ jxcore/_packaging/extract/go.sh
@@ -3,7 +3,10 @@
 rm -rf somefolder test.installed x.x
 
 BIN=C:\\Users\\azureuser\\Github\\jxcore_64v8\\Release\\jx.exe
-#BIN=jx
+BIN=C:\\jxcore\\jxcore_64v8\\Release\\jx.exe
+
+BIN=jx
+BIN=jx64v8305
 
 #jxsm package test.js --extract-what "*.txt" --extract-pre-actions "mkdir -p testfolder, JX_BINARY -jxv" --extract-app-root --extract-verbose
 #jx package test.js --extract-pre-actions "mkdir -p testfolder, JX_BINARY -jxv" --extract-verbose
