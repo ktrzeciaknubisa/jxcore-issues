ad882dadec4c0007f4b7bf69dff270768ed5c1ca
Krzysztof Trzeciak <ktrzeciak@nubisa.com>
2015-09-28 08:27:43 +0000

    from cloud

---
+++ jxcore/x564/test1.js
@@ -0,0 +1,7 @@
+/**
+ * Created by azureuser on 9/25/2015.
+ */
+
+//require('leveldown');
+//require('c:/users/azureuser/github/jxcore-issues/jxcore/x564/node_modules/leveldown/build/release/leveldown.node');
+require('C:/Users/azureuser/Github/jxcore-issues/jxcore/x564/node_modules/leveldown/build/Release/leveldown.node');
