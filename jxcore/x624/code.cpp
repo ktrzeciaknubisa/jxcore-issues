851cd39136132bdac57be9ff3cff63c972d331c3
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-10-30 16:42:44 +0100

    from mac

---
+++ jxcore/x624/code.cpp
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+ int fib(int x) {
+   if (x < 2) {
+     return 1;
+   } else {
+     return fib(x - 1) + fib(x - 2);
+   }
+ }
+
+ int main() {
+   int result = fib(45);
+
+   printf("%d\n", result);
+   return 1;
+ }
