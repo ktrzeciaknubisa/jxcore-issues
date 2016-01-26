5b20b4ac47cfcad7da79e8d713a9e1d13c04078a
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2016-01-26 16:15:10 +0100

    from mac

--- jxcore/_dev/npm/dev/prepare.sh
+++ jxcore/_dev/npm/dev/prepare.sh
@@ -2,10 +2,21 @@
 
 #scp npmi.sh root@nodejx.com:/var/www/vhosts/nubisacloud.com/nodejx/npmi.sh
 
-cd ..
-rm -rf npm.zip
-zip -r -9 npm.zip npm
+VER=311
+PWD=/Users/nubisa_krzs/Documents/GitHub/ktrzeciaknubisa/jxcore-issues/jxcore/_dev/npm
+DIR=$PWD
+#=/Users/nubisa_krzs/Documents/GitHub/ktrzeciaknubisa/jxcore-issues/jxcore/_dev/npm
 
-tar -zcvf npmjx310.tar.gz npm
+
+cd /Users/nubisa_krzs/Documents/GitHub/ktrzeciaknubisa/npm-diff
+rm -rf npm.zip npmjx$VER.tar.gz
+
+zip -r -9 npm.zip npm
+tar -zcvf npmjx$VER.tar.gz npm
+
+echo mv npm.zip $DIR/
+mv npm.zip $DIR/
+mv npmjx$VER.tar.gz $DIR/
+cd $DIR
 
 scp npm.zip root@nodejx.com:/var/www/vhosts/nubisacloud.com/nodejx/npm.zip
---
+++ jxcore/_dev/npm/dev/test-nodegyp.js
@@ -0,0 +1,3 @@
+// Copyright & License details are available under JXCORE_LICENSE file
+
+require('/Users/nubisa_krzs/Documents/GitHub/ktrzeciaknubisa/jxcore-issues/jxcore/_dev/npm/dev/node_modules/node-gyp/bin/node-gyp.js');
